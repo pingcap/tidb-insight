76bb38ba9a6e8d817d08dbf733b800af2b042988
Allen Zhong <zhongbenli@pingcap.com>
2018-07-12 18:52:09 +0800

    pdctl: add port to health and diagnose output of pdctl as well

--- measurement/tidb/pdctl.py
+++ measurement/tidb/pdctl.py
@@ -77,11 +77,11 @@ class PDCtl():
         pd_health = self.read_health()
         if pd_health:
             fileutils.write_file(os.path.join(
-                full_outputdir, "%s-health.json" % self.pd_host), pd_health)
+                full_outputdir, "%s_%s-health.json" % self.pd_host, self.pd_port), pd_health)
         pd_diagnose = self.read_diagnose()
         if pd_diagnose:
             fileutils.write_file(os.path.join(
-                full_outputdir, "%s-diagnose.json" % self.pd_host), pd_diagnose)
+                full_outputdir, "%s_%s-diagnose.json" % self.pd_host, self.pd_port), pd_diagnose)
 
         for key, info in self.read_runtime_info().items():
             if not info:
