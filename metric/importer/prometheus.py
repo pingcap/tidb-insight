49946f8e8500cd49648459ced1a52065e72b1360
Allen Zhong <zhongbenli@pingcap.com>
2018-08-28 17:56:49 +0800

    metric: cleanup code & handle write retries

--- metric/importer/prometheus.py
+++ metric/importer/prometheus.py
@@ -5,7 +5,6 @@
 # for futher analysis.
 
 import datetime
-import influxdb
 import json
 import logging
 import os
@@ -57,69 +56,39 @@ class PromDump():
             elif file.endswith('.dat'):
                 raw = zlib.decompress(fileopt.read_file(file, 'rb'))
             else:
-                logging.debug("Skipped unrecorgnized file '%s'" % file)
+                logging.debug("Skip unrecorgnized file '%s'" % file)
                 continue
             yield json.loads(raw)
 
-    def build_series(self):
-        def format_prom_metric(key=None):
-            points = []
-            point = {'fields': {}}
-            # build point header
-            for metric in key:
-                point['measurement'] = metric['metric']['__name__']
-                point['tags'] = {
-                    'cluster': self.db_name,
-                    'monitor': 'prometheus',
-                }
-                for k, v in metric['metric'].items():
-                    point['tags'][k] = v
-                # build point values
-                for value in metric['values']:
-                    point['time'] = datetime.datetime.utcfromtimestamp(
-                        value[0]).strftime('%Y-%m-%dT%H:%M:%SZ')
-                    try:
-                        point['fields']['value'] = float(value[1])
-                    except ValueError:
-                        point['fields']['value'] = value[1]
-                    points.append(point.copy())
-            return points
+    def exec_importer(self, data, chunk_size=2000):
+        base_dir = os.path.join(util.pwd(), "../")
+        importer = os.path.join(base_dir, "bin/prom2influx")
+        cmd = [importer,
+               "-db", self.db_name,
+               "-host", self.host,
+               "-port", "%s" % self.port,
+               "-chunk", "%s" % chunk_size,  # chunk size of one write request
+               ]
+        logging.debug("Running cmd: %s" % ' '.join(cmd))
+        return util.run_cmd(cmd, input=json.dumps(data).encode('utf-8'))
 
-        for metric in self.load_dump():
-            if not metric:
-                continue
-            fileopt.write_file("test", json.dumps(metric))
-            cmd = ["/home/allen/dev/pingcap/tidb-insight/tools/prom2influx",
-                        "-db", self.db_name,
-                        "-host", self.host,
-                        "-port", str(self.port)]
-            logging.debug("Running cmd: %s" % ' '.join(cmd))
-            stdout, stderr = util.run_cmd(cmd, input=json.dumps(metric))
-            logging.debug("stdout: %s\nstderr: %s" % (len(stdout), len(stderr)))
-            if stderr:
-                logging.warn(stderr)
-            yield format_prom_metric(metric)
-
-    def write2influxdb(self):
-        #client = influxdb.InfluxDBClient(
-        #    host=self.host, port=self.port, username=self.user, password=self.passwd,
-        #    database=self.db_name, timeout=30)
-        # create_database has no effect if the database already exist
-        #client.create_database(self.db_name)
+    def run_importing(self):
         logging.info("Metrics will be imported to database '%s'." %
                      self.db_name)
 
-        for series in self.build_series():
+        for series in self.load_dump():
             if not series:
                 continue
-            try:
-                print("series size: %s" % len(series))
-                #fileopt.write_file("test", json.dumps(series))
-                #client.write_points(series, batch_size=2000)
-            except influxdb.exceptions.InfluxDBClientError as e:
-                logging.warn(
-                    "Write error for key '%s', data may be empty." % series[0]['measurement'])
-                logging.debug(e)
-
-    def run_importing(self):
-        self.write2influxdb()
+            stderr = self.exec_importer(series)[1]
+            if stderr and "Request Entity Too Large" in stderr.decode('utf-8'):
+                metric_name = series[0]["metric"]["__name__"]
+                logging.info("Write to DB failed, retry for once...")
+                retry_stderr = self.exec_importer(series, chunk_size=100)[1]
+                if not retry_stderr:
+                    logging.info("Retry succeeded for metric '%s'." %
+                                 metric_name)
+                else:
+                    logging.warn(
+                        "Retry failed for metric '%s', stderr is: '%s'" % metric_name, retry_stderr)
+            elif stderr:
+                logging.warn(stderr)
