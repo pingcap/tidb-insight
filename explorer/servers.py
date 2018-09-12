28e470f78b435817fad3073189321ae362729ac4
Allen Zhong <zhongbenli@pingcap.com>
2018-09-12 16:24:48 +0800

    explorer: basic output of inventory file info

--- explorer/servers.py
+++ explorer/servers.py
@@ -10,16 +10,33 @@ from utils import util
 
 class TUIServers(tui.TUIBase):
     def __init__(self, args):
-        # init file list
+        # init file list and inventory
         super(TUIServers, self).__init__(args)
 
-    # adjust column width to fit the longest content
-    def format_columns(self, data):
-        result = []
-        col_width = max(len(word) for row in data for word in row) + 2
-        for row in data:
-            result.append("".join(word.ljust(col_width) for word in row))
-        return result
+        # map roles to hosts
+        self.host_roles = {}
+        for group, hosts in self.inventory.get_groups_dict().items():
+            if group == 'all':
+                continue
+            role = group[:-8] if group.endswith('_servers') else group
+            # monitored_servers are all hosting servers, other roles may be
+            # instances on hosting servers
+            if group == 'monitored_servers':
+                role = 'host'
+            for host in hosts:
+                try:
+                    self.host_roles[host].add(role)
+                except KeyError:
+                    self.host_roles[host] = set()
+                    self.host_roles[host].add(role)
 
     def display(self):
-        print(self.inventory.get_groups_dict())
+        output = [['Server', 'Roles']]
+        for host, roles in self.host_roles.items():
+            row = [host]
+            role_list = list(roles)
+            role_list.sort()
+            row.append(','.join(role_list))
+            output.append(row)
+        for row in self.format_columns(output):
+            print(row)
