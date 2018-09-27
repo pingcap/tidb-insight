6d69ab47f4b9c255ee26086140bd4aae5ab2961d
Allen Zhong <zhongbenli@pingcap.com>
2018-09-27 17:32:26 +0800

    explorer: output basic tikv info

--- explorer/modules.py
+++ explorer/modules.py
@@ -25,6 +25,8 @@ class TUIModule(TUIModuleBase):
 
         if args.subcmd_show == 'tidb':
             self.module = TUIModuleTiDB(args)
+        elif args.subcmd_show == 'tikv':
+            self.module = TUIModuleTiKV(args)
 
     def display(self):
         self.module.display()
@@ -43,7 +45,7 @@ class TUIModuleTiDB(TUIModuleBase):
                 key = file.split('-tidb-')[-1][:-5]
                 self.tidbinfo[host][key] = json.loads(fileopt.read_file(file))
 
-    def build_module_info(self):
+    def build_tidb_info(self):
         result = []
 
         info = []
@@ -83,7 +85,61 @@ class TUIModuleTiDB(TUIModuleBase):
         return result
 
     def display(self):
-        for section in self.build_module_info():
+        for section in self.build_tidb_info():
+            print('')
+            for row in self.format_columns(section):
+                print(row)
+
+
+class TUIModuleTiKV(TUIModuleBase):
+    def __init__(self, args):
+        super(TUIModuleTiKV, self).__init__(args)
+        self.tikvinfo = {}
+
+    def build_tikv_info(self):
+        result = []
+
+        info = []
+        info.append(['Host', 'AdvAddr', 'Version',
+                     'PD', 'MemRSS', 'VMS', 'Swap'])
+        for host in self.hosts:
+            host = str(host)
+            _proc = None
+            for proc in self.collector[host]['proc_stats']:
+                if 'tikv' in proc['name']:
+                    _proc = proc
+            if not _proc:
+                continue
+
+            _cmd = _proc['cmd'].split()
+            try:
+                adv_addr = _cmd[_cmd.index('--advertise-addr') + 1]
+            except ValueError:
+                adv_addr = ''
+            try:
+                pd_addr = _cmd[_cmd.index('--pd') + 1]
+            except ValueError:
+                pd_addr = ''
+            try:
+                rel_ver = '%s-g%s' % (self.collector[host]['meta']['tikv']['release_version'],
+                                      self.collector[host]['meta']['tikv']['git_commit'][:6])
+            except KeyError:
+                rel_ver = ''
+            info.append([
+                host, adv_addr, rel_ver, pd_addr,
+                util.format_size_bytes(
+                    _proc['memory']['rss']) if _proc else '',
+                util.format_size_bytes(
+                    _proc['memory']['vms']) if _proc else '',
+                util.format_size_bytes(
+                    _proc['memory']['swap']) if _proc else ''
+            ])
+        result.append(info)
+
+        return result
+
+    def display(self):
+        for section in self.build_tikv_info():
             print('')
             for row in self.format_columns(section):
                 print(row)
