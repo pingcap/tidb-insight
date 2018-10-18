dbc66f31a8332123f2fcbb789daa21a41c092dd5
Allen Zhong <zhongbenli@pingcap.com>
2018-10-18 15:25:50 +0800

    collector: add system uptime

--- collector/utils.go
+++ collector/utils.go
@@ -1,5 +1,11 @@
 package main
 
+import (
+	"io/ioutil"
+	"strconv"
+	"strings"
+)
+
 // Version infomation
 var (
 	// InsightGitBranch is initialized during make
@@ -11,3 +17,21 @@ var (
 	// InsightBuildDate is initialized during make
 	InsightBuildTime = "Not Provided"
 )
+
+func GetSysUptime() (float64, float64, error) {
+	contents, err := ioutil.ReadFile("/proc/uptime")
+	if err != nil {
+		return 0, 0, err
+	}
+	timeStrings := strings.Split(string(contents), "\n")
+	timerCounts := strings.Split(string(timeStrings[0]), " ")
+	uptime, err := strconv.ParseFloat(timerCounts[0], 64)
+	if err != nil {
+		return 0, 0, err
+	}
+	idleTime, err := strconv.ParseFloat(timerCounts[1], 64)
+	if err != nil {
+		return 0, 0, err
+	}
+	return uptime, idleTime, err
+}
