8dad166491c2595b6916713e7f589b65ed95b232
Qiμ$hiЯuí <39378935+srstack@users.noreply.github.com>
2022-01-04 17:50:42 +0800

    collector: support collecting chrony info (#56)

--- collector/insight/insight.go
+++ collector/insight/insight.go
@@ -42,6 +42,7 @@ type InsightInfo struct {
 	Meta       Meta            `json:"meta"`
 	SysInfo    sysinfo.SysInfo `json:"sysinfo,omitempty"`
 	NTP        TimeStat        `json:"ntp,omitempty"`
+	ChronyStat ChronyStat      `json:"chrony,omitempty"`
 	Partitions []BlockDev      `json:"partitions,omitempty"`
 	ProcStats  []ProcessStat   `json:"proc_stats,omitempty"`
 	EpollExcl  bool            `json:"epoll_exclusive,omitempty"`
@@ -70,6 +71,7 @@ func (info *InsightInfo) GetInfo(opts Options) {
 	} else {
 		info.SysInfo.GetSysInfo()
 		info.NTP.getNTPInfo()
+		info.ChronyStat.getChronyInfo()
 		info.Partitions = GetPartitionStats()
 		switch runtime.GOOS {
 		case "android",
